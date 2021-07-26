49cabe1b7abcbe70369d8b1ac101242b306d9680
Paul Butler <paulgb@gmail.com>
2021-07-26 15:11:23 +0000

    more complete room creation strategy

--- jamsocket-cli/src/serve.rs
+++ jamsocket-cli/src/serve.rs
@@ -1,7 +1,7 @@
 use crate::cli_opts::ServeCommand;
 use crate::room_id::{RoomIdGenerator, RoomIdStrategy, UuidRoomIdGenerator};
 use actix::{Actor, Addr};
-use actix_web::error::{ErrorBadRequest, ErrorInternalServerError, ErrorNotFound};
+use actix_web::error::{ErrorBadRequest, ErrorInternalServerError};
 use actix_web::web;
 use actix_web::{get, post, web::Data, App, Error, HttpRequest, HttpResponse, HttpServer, Result};
 use async_std::sync::RwLock;
@@ -23,14 +23,30 @@ async fn status() -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().body("ok"))
 }
 
+async fn try_create_room(room_id: String, room_mapper: &RoomMapper, host_factory: &Data<WasmHostFactory>) -> Option<Addr<RoomActor>> {
+    match room_mapper.write().await.entry(room_id) {
+        std::collections::hash_map::Entry::Occupied(_) => None,
+        std::collections::hash_map::Entry::Vacant(entry) => {
+            let service_constructor: Box<dyn FnOnce(ServiceActorContext) -> WasmHost> = {
+                let wasm_host_factory = host_factory.clone();
+                Box::new(move |wctx| wasm_host_factory.create_room(wctx))
+            };
+
+            let room_addr = ServiceActor::create(|ctx| ServiceActor::new(ctx, service_constructor).unwrap())
+                .send(GetRoomAddr)
+                .await
+                .unwrap();
+
+            entry.insert(room_addr.clone());
+                
+            Some(room_addr)
+        },
+    }
+}
+
 #[post("/new_room")]
 async fn new_room(req: HttpRequest) -> Result<HttpResponse, Error> {
     let wasm_host_factory: &Data<WasmHostFactory> = req.app_data().unwrap();
-
-    let service_constructor: Box<dyn FnOnce(ServiceActorContext) -> WasmHost> = {
-        let wasm_host_factory = wasm_host_factory.clone();
-        Box::new(move |wctx| wasm_host_factory.create_room(wctx))
-    };
 
     let room_id = {
         let room_id_strategy: &Data<RoomIdStrategy> = req.app_data().unwrap();
@@ -46,14 +62,9 @@ async fn new_room(req: HttpRequest) -> Result<HttpResponse, Error> {
         }
     };
 
-    let room_addr =
-        ServiceActor::create(|ctx| ServiceActor::new(ctx, service_constructor).unwrap())
-            .send(GetRoomAddr)
-            .await
-            .unwrap();
-
     let room_mapper: &Data<RoomMapper> = req.app_data().unwrap();
-    room_mapper.write().await.insert(room_id.clone(), room_addr);
+    // TODO: this could fail, we really need to wrap it in a `try_generate_room`.
+    try_create_room(room_id.clone(), room_mapper, wasm_host_factory).await.unwrap();
 
     Ok(HttpResponse::Ok().json(NewRoom { room_id }))
 }
@@ -66,12 +77,26 @@ async fn websocket(
 ) -> actix_web::Result<HttpResponse> {
     let room_mapper: &Data<RoomMapper> = req.app_data().unwrap();
 
-    let room_addr = room_mapper
-        .read()
-        .await
-        .get(room_id.as_ref())
-        .ok_or(ErrorNotFound("Room not found."))?
-        .clone();
+    let room_addr = if let Some(room_addr) = room_mapper.read().await.get(room_id.as_ref()) {
+        room_addr.clone()
+    } else {
+        let room_id_strategy: &Data<RoomIdStrategy> = req.app_data().unwrap();
+
+        if let &RoomIdStrategy::Implicit = room_id_strategy.get_ref() {
+            // TODO: there is technically a race condition where if a room does not exist when
+            // we first try to read it, but it is created before we get the write lock, we will
+            // fail to connect to the room when the correct behavior is to connect to the existing
+            // room.
+            let wasm_host_factory: &Data<WasmHostFactory> = req.app_data().unwrap();
+            let room_addr = try_create_room(room_id.to_string(), room_mapper, wasm_host_factory);
+
+            room_addr.await.unwrap()
+        } else {
+            return Err(ErrorBadRequest(
+                "The requested room was not found.",
+            ))
+        }
+    };
 
     let user = room_addr
         .send(AssignUserId)
