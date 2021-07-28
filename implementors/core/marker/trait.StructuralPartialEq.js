11d204246581e7696db7a39fe084ad76d3ef1fc5
paulgb <paulgb@gmail.com>
2021-07-28 21:39:59 +0000

    Deploying to gh-pages from @ jamsocket/jamsocket@03df5829b9f593198befbd0e4ebee18fa35e0cf0 🚀

---
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["jamsocket"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"jamsocket/enum.MessageRecipient.html\" title=\"enum jamsocket::MessageRecipient\">MessageRecipient</a>","synthetic":false,"types":["jamsocket::message_recipient::MessageRecipient"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
