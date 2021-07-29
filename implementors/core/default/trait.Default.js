474d6ea5bf9cdc3e52a2d0b5fc9c51efac1b7308
paulgb <paulgb@gmail.com>
2021-07-29 15:09:24 +0000

    Deploying to gh-pages from @ jamsocket/jamsocket@2fd8f7433bfd746db6b263792cbb0c0f32fb6d55 🚀

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["jamsocket_server"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"jamsocket_server/enum.RoomIdStrategy.html\" title=\"enum jamsocket_server::RoomIdStrategy\">RoomIdStrategy</a>","synthetic":false,"types":["jamsocket_server::room_id::RoomIdStrategy"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
