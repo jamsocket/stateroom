cb662e12fa18522209877144d6cedd22f38245ad
paulgb <paulgb@gmail.com>
2021-07-30 17:17:35 +0000

    Deploying to gh-pages from @ jamsocket/jamsocket@4db2f3af207328482c30f900bc6b43b43983ad95 🚀

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["jamsocket_server"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"jamsocket_server/enum.ServiceShutdownPolicy.html\" title=\"enum jamsocket_server::ServiceShutdownPolicy\">ServiceShutdownPolicy</a>","synthetic":false,"types":["jamsocket_server::shutdown_policy::ServiceShutdownPolicy"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
