279fc4621d9efde2bd7a1d23485c7b1f20a75ebd
paulgb <paulgb@gmail.com>
2021-07-30 16:03:41 +0000

    Deploying to gh-pages from @ jamsocket/jamsocket@ae15ad6a158dbaafcb28773629fbc42da52dc204 🚀

--- source-files.js
+++ source-files.js
@@ -1,7 +1,7 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["jamsocket"] = {"name":"","files":["lib.rs","message_recipient.rs"]};
 sourcesIndex["jamsocket_cli"] = {"name":"","files":["cli_opts.rs","main.rs","serve.rs"]};
-sourcesIndex["jamsocket_server"] = {"name":"","files":["client_socket_connection.rs","lib.rs","messages.rs","room_actor.rs","room_id.rs","service_actor.rs"]};
+sourcesIndex["jamsocket_server"] = {"name":"","files":["client_socket_connection.rs","lib.rs","messages.rs","room_actor.rs","room_id.rs","server_state.rs","service_actor.rs"]};
 sourcesIndex["jamsocket_wasm"] = {"name":"","files":["lib.rs","prelude.rs"]};
 sourcesIndex["jamsocket_wasm_host"] = {"name":"","files":["lib.rs","wasm_host.rs","wasm_host_factory.rs"]};
 sourcesIndex["jamsocket_wasm_macro"] = {"name":"","files":["lib.rs"]};
