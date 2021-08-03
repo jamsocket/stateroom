e57f13530cf267c38ce402ac29e51da08d3b25bb
Paul Butler <paulgb@gmail.com>
2021-08-03 18:43:36 +0000

    add "cpu hog" example

---
+++ examples/cpu-hog/src/lib.rs
@@ -0,0 +1,39 @@
+use jamsocket_wasm::prelude::*;
+
+// Seconds per nanosecond. (`wasi::clock_time_get` uses nanos.)
+const SECONDS: u64 = 1_000_000_000;
+
+#[jamsocket_wasm]
+struct CpuHog(String);
+
+fn get_time() -> u64 {
+    unsafe {
+        wasi::clock_time_get(wasi::CLOCKID_REALTIME, 0).unwrap()
+    }
+}
+
+impl SimpleJamsocketService for CpuHog {
+    fn new(room_id: &str, _: &impl JamsocketContext) -> Self {
+        CpuHog(room_id.to_string())
+    }
+
+    fn connect(&mut self, _: u32, ctx: &impl JamsocketContext) {
+        ctx.send_message(
+            MessageRecipient::Broadcast,
+            &format!("Connected to room {}", self.0),
+        );
+        
+        let init_time = get_time();
+        loop {
+            let cur_time = get_time();
+            if init_time + 10 * SECONDS < cur_time {
+                break;
+            }
+        }
+    
+        ctx.send_message(
+            MessageRecipient::Broadcast,
+            &format!("Finished in room {}", self.0),
+        );
+    }
+}
