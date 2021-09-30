c9820ee008a14b591a9cdcc7f9f3f786ba2554ac
Paul Butler <paulgb@gmail.com>
2021-09-30 12:29:07 -0400

    init works if jamsocket.toml doesn't exist

--- jamsocket-cli/src/commands/init.rs
+++ jamsocket-cli/src/commands/init.rs
@@ -1,46 +1,47 @@
-use crate::config::{GlobalConfigHandle, JamsocketConfig, ServiceConfig};
+use crate::config::{GlobalConfigHandle};
 use colored::Colorize;
 use jamsocket_api::JamsocketApi;
-use jamsocket_server::{RoomIdStrategy, ServiceShutdownPolicy};
-use std::{fs::File, path::Path};
-use std::io::prelude::Write;
+use std::fs::read_to_string;
+use toml_edit::{Document, value};
+
+const JAMSOCKET_TOML: &str = "jamsocket.toml";
+const SERVICE_ID: &str = "service_id";
 
 pub fn init() -> anyhow::Result<()> {
-    if Path::new("jamsocket.toml").exists() {
+    let global_config = GlobalConfigHandle::new()?;
+    let doc_str = match read_to_string(JAMSOCKET_TOML) {
+        Ok(v) => v,
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => "".to_string(),
+        Err(e) => return Err(e.into())
+    };
+    let mut doc = doc_str.parse::<Document>()?;
+
+    if doc[SERVICE_ID].is_str() {
         println!(
             "{}",
-            "jamsocket.toml file already exists, not overwriting"
+            "jamsocket.toml file already has a service_id, not overwriting"
                 .red()
                 .bold()
         );
         return Ok(());
     }
 
-    let global_config = GlobalConfigHandle::new()?;
-
-    if let Some(token) = global_config.config.token {
-        let service_id = JamsocketApi::new(&token).new_service()?;
-        let service_config = JamsocketConfig {
-            static_files: None,
-            client: None,
-            service: ServiceConfig {
-                package: None,
-                room_strategy: RoomIdStrategy::Explicit,
-                shutdown_policy: ServiceShutdownPolicy::Immediate,
-            },
-            service_id: Some(service_id.clone()),
-        };
-
-        let mut file = File::create("jamsocket.toml")?;
-        file.write_all(&toml::to_vec(&service_config)?)?;
-
-        println!(
-            "Created new service {} and wrote to jamsocket.toml.",
-            service_id.green().bold()
-        );
+    let token = if let Some(token) =  global_config.config.token {
+        token
     } else {
-        println!("Need to be logged in.")
-    }
+        println!("Need to be logged in.");
+        return Ok(())
+    };
+
+    let service_id = JamsocketApi::new(&token).new_service()?;
+
+    doc[SERVICE_ID] = value(&service_id);
+    std::fs::write(JAMSOCKET_TOML, doc.to_string())?;
+
+    println!(
+        "Created new service {} and wrote to jamsocket.toml.",
+        service_id.green().bold()
+    );
 
     Ok(())
 }
